5f57f2b1d754d2999811df1ad1c3ceaee023879a
Jon Coello <joncoello@hotmail.com>
2015-11-01 21:51:30 +0000

    Add main area form

---
+++ CS.UI.Client/ClientListForm/frmClientList.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace CS.UI.Client.ClientListForm
+{
+    partial class frmClientList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdList = new Janus.Windows.GridEX.GridEX();
+            ((System.ComponentModel.ISupportInitialize)(this.grdList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdList
+            // 
+            this.grdList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grdList.Location = new System.Drawing.Point(12, 12);
+            this.grdList.Name = "grdList";
+            this.grdList.Size = new System.Drawing.Size(652, 482);
+            this.grdList.TabIndex = 0;
+            // 
+            // frmClientList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(676, 506);
+            this.Controls.Add(this.grdList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmClientList";
+            this.Text = "frmClientList";
+            ((System.ComponentModel.ISupportInitialize)(this.grdList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Janus.Windows.GridEX.GridEX grdList;
+    }
+}
---
+++ CS.UI.Client/ClientListForm/frmClientList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CS.UI.Common.FormFactory;
+using MYOB.CSSInterface;
+using MYOB.CSS;
+using Janus.Windows.GridEX;
+
+namespace CS.UI.Client.ClientListForm
+{
+    public partial class frmClientList : Form, ICSSDisplayMainArea
+    {
+        public frmClientList(ClientList factory)
+        {
+            InitializeComponent();
+        }
+
+        public int CollectionID { get; set; }
+
+        public void CloseForm(object sender, CSSCancelEventArgs e)
+        {
+            this.Close();
+        }
+
+        public SideBarGroups Register()
+        {
+            this.Show();
+            return new SideBarGroups("");
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            InitGrid();
+            LoadData();
+
+        }
+
+        private void InitGrid()
+        {
+
+            GridEXColumn col;
+
+            var rt = new GridEXTable();
+
+            grdList.RootTable = rt;
+
+            rt.Columns.Add("Name");
+            rt.Columns.Add("Field1");
+            rt.Columns.Add("Field2");
+
+            CssContext.Instance.Host.Personalisation.RestoreGridLayout("mygrid", grdList);
+        }
+
+        private void LoadData()
+        {
+            var data = new DataTable();
+            grdList.SetDataBinding(data, string.Empty);
+        }
+
+    }
+}
