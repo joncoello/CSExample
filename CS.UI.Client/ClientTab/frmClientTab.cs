ee11edada998ed5116b82b101daa7a590a120b9e
Jon Coello <joncoello@hotmail.com>
2015-11-01 21:06:30 +0000

    Initial checkin of plugin with client tab and homepage control

---
+++ CS.UI.Client/ClientTab/frmClientTab.cs
@@ -0,0 +1,56 @@
+﻿using MYOB.CSSInterface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS.UI.Client.ClientTab
+{
+    public partial class frmClientTab : Form, ICSSClientFormPlugIn
+    {
+        public frmClientTab()
+        {
+            InitializeComponent();
+        }
+
+        public int Position
+        {
+            get
+            {
+                return 3;
+            }
+        }
+
+        public bool CheckSecurityPermission(AvailableArea Origin)
+        {
+            return true;
+        }
+
+        public void CloseForm(object sender, CSSChildEventArgs e)
+        {
+            
+        }
+
+        public void LoadClient(int ClientId, ICSSHost Host)
+        {
+            
+        }
+
+        public void RefreshForm()
+        {
+            
+        }
+
+        public void SaveChanges(object sender, CSSChildSaveEventArgs e)
+        {
+            
+        }
+
+    }
+
+}
---
+++ CS.UI.Homepage/Homepagebase.cs
@@ -0,0 +1,55 @@
+﻿using MYOB.CSSInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CS.UI.Homepage
+{
+    public class Homepagebase : HomePageItem
+    {
+
+        internal Homepagebase()
+        {
+
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                return "Clear Sky";
+            }
+        }
+
+        public override string DisplayName
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override void DefaultSettings()
+        {
+            
+        }
+
+        public override void LoadData(bool Cache, bool StartNewThread)
+        {
+            
+        }
+
+        public override void RestoreCustomisation(XmlElement Customisation)
+        {
+            
+        }
+
+        public override void SaveCustomisation(XmlTextWriter XW)
+        {
+            
+        }
+    }
+}
