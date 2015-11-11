b40ccf2a1a521cc0373d6ba020aa14c8393a14d1
Jon Coello <joncoello@hotmail.com>
2015-11-11 15:27:14 +0000

    add document model to source

---
+++ CS.DomainModel/Models/Documents/Document.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.DomainModel.Models.Documents
+{
+    public class Document
+    {
+        public int DocumentID { get; set; }
+        public string Name { get; set; }
+    }
+}
