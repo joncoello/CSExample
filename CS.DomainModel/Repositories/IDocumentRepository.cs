6952cca7e5d324b4900b6d491ac33359bdc799a3
Jon Coello <joncoello@hotmail.com>
2015-11-11 15:29:37 +0000

    add doc repo to domain model

---
+++ CS.DomainModel/Repositories/IDocumentRepository.cs
@@ -0,0 +1,17 @@
+﻿using CS.DomainModel.Models.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.DomainModel.Repositories
+{
+    public interface IDocumentRepository
+    {
+        IEnumerable<Document> GetDocuments();
+        void DownloadDocument(int documentID, string path);
+        void UploadDocument(string path);
+
+    }
+}
