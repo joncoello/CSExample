b85522a9d1556686962c9fa490fca44bb2ec40ff
Jon Coello <joncoello@hotmail.com>
2015-11-11 15:49:19 +0000

    include missing formfactory

---
+++ CS.UI.Common/FormFactory/ClientList.cs
@@ -0,0 +1,37 @@
+﻿using MYOB.CSSInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.UI.Common.FormFactory
+{
+    public class ClientList : FrameworkForm
+    {
+
+        [SkipFrameworkComparision]
+        public Guid Arg1 { get; set; }
+
+        public ClientList(Guid myarg1)
+        {
+            Arg1 = myarg1;
+        }
+
+        protected override string AssemblyName
+        {
+            get
+            {
+                return "CS.UI.Client.ClientListForm";
+            }
+        }
+
+        protected override string ClassName
+        {
+            get
+            {
+                return "frmClientList";
+            }
+        }
+    }
+}
