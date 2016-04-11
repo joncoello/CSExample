50eae19781b51d1de1383d4fd53068a66696ae47
Jon Coello <joncoello@hotmail.com>
2016-04-11 13:08:47 +0100

    add hook of contact loading/closed events

---
+++ CS.UI.Client/CentralApplication.cs
@@ -0,0 +1,52 @@
+﻿using MYOB.CSS;
+using MYOB.CSSInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS.UI.Client
+{
+    public class CentralApplication : CSSApplication
+    {
+        public CentralApplication()
+        {
+            this.Startup += CentralApplication_Startup;
+            this.ShutDown += CentralApplication_ShutDown;
+        }
+        
+        private void CentralApplication_Startup(object sender, EventArgs e)
+        {
+            CSSFormEventHandler.Instance.AddHandle(CSSFormEventHandler.CSSEventArea.Contact, CSSFormEventHandler.CSSFormEvent.Loading, ContactLoading);
+            CSSFormEventHandler.Instance.AddHandle(CSSFormEventHandler.CSSEventArea.Contact, CSSFormEventHandler.CSSFormEvent.Closed, ContactClosed);
+        }
+        
+        private void CentralApplication_ShutDown(object sender, EventArgs e)
+        {
+            CSSFormEventHandler.Instance.RemoveHandle(CSSFormEventHandler.CSSEventArea.Contact, CSSFormEventHandler.CSSFormEvent.Loading, ContactLoading);
+            CSSFormEventHandler.Instance.RemoveHandle(CSSFormEventHandler.CSSEventArea.Contact, CSSFormEventHandler.CSSFormEvent.Closed, ContactClosed);
+        }
+
+        private void ContactLoading(object sender, EventArgs e)
+        {
+            var eArgs = e as FrameworkEventArgs;
+            if (eArgs != null)
+            {
+                MessageBox.Show($"CS plugin hooked contact {eArgs.PropertyBag.ContactId} loading");
+            }
+        }
+
+        private void ContactClosed(object sender, EventArgs e)
+        {
+            var eArgs = e as FrameworkEventArgs;
+            if (eArgs != null)
+            {
+                MessageBox.Show($"CS plugin hooked contact {eArgs.PropertyBag.ContactId} closed");
+            }
+        }
+
+
+    }
+}
